ca2c492841ce52b429d2841214362ab3936cf509
msnadms <adamsmb@purdue.edu>
2021-11-22 18:48:24 -0500

    added clicking functionality that is consistent

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
+import math
 import time
 import cv2
 import mediapipe as mp
 import win32api as wp
-import win32con
+import win32con as wc
 WIN_NAME = 'Picture'
+MHEIGHT = wp.GetSystemMetrics(1)
+MWIDTH = wp.GetSystemMetrics(0)
+
+
+def get_distance(first, second):
+    x1, y1 = first
+    x2, y2 = second
+    return math.sqrt((x1-x2)**2 + (y1-y2)**2)
 
 
 def track():
@@ -33,15 +42,27 @@ def track():
         if sensed:
             for mano in sensed:
                 lm = mano.landmark[8]  # tip of index finger
-                cursor_pos = mano.landmark[12]  # tip of middle finger
+                cur = mano.landmark[12]  # tip of middle finger
+                compare = mano.landmark[3]  # thumb knuckle
                 lm_c_one = mano.landmark[1]  # base of thumb
                 lm_c_two = mano.landmark[0]  # wrist
                 cx, cy = int(lm.x * width), int(lm.y * height)
-                mx, my = int(cursor_pos.x * width), int(cursor_pos.y * height)
-                wp.SetCursorPos((mx, my))
-                clicked_color = (255, 0, 0) if lm.z > lm_c_one.z and lm.z > lm_c_two.z else (255, 0, 255)
+                mx, my = int(compare.x * width), int(compare.y * height)
+                distance = get_distance((cur.x * width, cur.y * height), (mx, my))
+
+                # Translate to monitor specifications
+                mtrans_w = lm.x * MWIDTH
+                mtrans_h = lm.y * MHEIGHT
+                wp.SetCursorPos((int(mtrans_w), int(mtrans_h)))
+
+                if distance <= 50:
+                    clicked_color = (255, 0, 0)
+                    wp.mouse_event(wc.MOUSEEVENTF_LEFTDOWN, int(mtrans_w), int(mtrans_h), 0, 0)
+                else:
+                    clicked_color = (255, 0, 255)
+                    wp.mouse_event(wc.MOUSEEVENTF_LEFTUP, int(mtrans_w), int(mtrans_h), 0, 0)
+
                 cv2.circle(img, (cx, cy), 5, clicked_color, cv2.FILLED)
-                # draw.draw_landmarks(img, mano)
 
         cur_time = time.time()
         fps = 1 / (cur_time - prev_time)
