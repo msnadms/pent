7711708e97ab099639a4438b2e67869ee3bdd472
msnadms <adamsmb@purdue.edu>
2021-11-22 16:17:14 -0500

    added skeleton of clicking and mouse movement functionality

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+import time
 import cv2
 import mediapipe as mp
-import time
+import win32api as wp
+import win32con
+WIN_NAME = 'Picture'
 
 
 def track():
@@ -10,12 +13,17 @@ def track():
     prev_time = 0
     cur_time = 0
 
+    cv2.namedWindow(WIN_NAME, cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty(WIN_NAME, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    _, overlay = cap.read()
+    height, width, channel = overlay.shape
+
     record = True
     while record:
         exito, img = cap.read()
         img.flags.writeable = False
         img = cv2.flip(img, 1)
-        height, width, channel = img.shape
         if not exito:
             print("Error! Wrong input device!")
             break
@@ -24,9 +32,15 @@ def track():
         sensed = results.multi_hand_landmarks
         if sensed:
             for mano in sensed:
-                lm = mano.landmark[8]
+                lm = mano.landmark[8]  # tip of index finger
+                cursor_pos = mano.landmark[12]  # tip of middle finger
+                lm_c_one = mano.landmark[1]  # base of thumb
+                lm_c_two = mano.landmark[0]  # wrist
                 cx, cy = int(lm.x * width), int(lm.y * height)
-                cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+                mx, my = int(cursor_pos.x * width), int(cursor_pos.y * height)
+                wp.SetCursorPos((mx, my))
+                clicked_color = (255, 0, 0) if lm.z > lm_c_one.z and lm.z > lm_c_two.z else (255, 0, 255)
+                cv2.circle(img, (cx, cy), 5, clicked_color, cv2.FILLED)
                 # draw.draw_landmarks(img, mano)
 
         cur_time = time.time()
@@ -35,8 +49,9 @@ def track():
 
         cv2.putText(img, str(int(fps)), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 1)
 
-        cv2.imshow("Picture", img)
-        cv2.waitKey(1)
+        cv2.imshow(WIN_NAME, img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
 def main():
